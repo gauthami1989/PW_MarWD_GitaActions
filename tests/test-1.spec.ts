94eae121379ef05919799bf837db4ed506696e89
gauthami1989 <gauthamshara@gmail.com>
2026-05-07 19:19:24 +0530

    Version new

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://leaftaps.com/opentaps/control/main');
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill('demosales');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('crmsfa');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.locator('#errorDiv')).toContainText('following error occurred during login: User not found.');
+});
